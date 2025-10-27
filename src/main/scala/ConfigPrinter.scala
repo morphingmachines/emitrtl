32af55723f4b3f7d7753ad238fdfbd4d7a98549d
madhava krishna <madhav@morphing.in>
2025-10-27 10:42:19 +0530

    ✨ feat: Add API to delete existing parameters file.

--- src/main/scala/ConfigPrinter.scala
+++ src/main/scala/ConfigPrinter.scala
@@ -6,10 +6,11 @@ import java.nio.file.{Files, Paths}
 // Helper functions to print the module parameters
 
 object ConfigPrinter {
+  val paramsFileName = "parameters.txt"
 
-  // Iterates over the Product and appends the parameters to parameters.txt. BigInts are printed in Hex
+  /** Iterates over the Product and appends the parameters to [[paramsFileName]]. BigInts are printed in Hex */
   def printParams(header: String, params: Product): Unit = {
-    val writer = new PrintWriter(new FileWriter("parameters.txt", true))
+    val writer = new PrintWriter(new FileWriter(paramsFileName, true))
     writer.append("********" + header + "********\n")
     params.productElementNames.zip(params.productIterator).foreach { case (name, value) =>
       val valueStr = value match {
@@ -21,17 +22,26 @@ object ConfigPrinter {
     writer.append(s"\n")
     writer.close()
   }
-  // overloaded function to append any custom string format to parameters.txt
+
+  /** overloaded function to append any custom string format to [[paramsFileName]]. */
   def printParams(printString: String): Unit = {
-    val writer = new PrintWriter(new FileWriter("parameters.txt", true))
+    val writer = new PrintWriter(new FileWriter(paramsFileName, true))
     writer.append(printString + "\n")
     writer.close()
   }
-  // move the generated parameters.txt to out_dir (generated_sv_dir/topModule/)
+
+  /** move the generated [[paramsFileName]] to out_dir (generated_sv_dir/topModule/) */
   def moveParametersFile(out_dir: String): Unit = {
-    val path = Paths.get("parameters.txt")
+    val path = Paths.get(paramsFileName)
     if (Files.exists(path)) {
-      path.toFile().renameTo(Paths.get(out_dir, "parameters.txt").toFile())
+      path.toFile().renameTo(Paths.get(out_dir, paramsFileName).toFile())
+    }
+  }
+
+  def deleteParametersFile: Unit = {
+    val path = Paths.get(paramsFileName)
+    if (Files.exists(path)) {
+      Files.delete(path)
     }
   }
 
